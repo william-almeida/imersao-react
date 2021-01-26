8ed5b019d44eb148b604ad97d764af02ec7aae96
william-almeida <william.cntt@gmail.com>
2021-01-26 01:03:15 -0300

    Theme quiz: Lil peep

--- quiz/pages/index.js
+++ quiz/pages/index.js
@@ -1,10 +1,6 @@
 import styled from 'styled-components'
 import db from '../db.json'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
 const BackgroundImage = styled.div`
   background-image: url(${db.bg});
   flex: 1;
@@ -14,12 +10,23 @@ const BackgroundImage = styled.div`
   background-size: cover;
   background-position: center;
 `
+export const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media sreen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
+`
 
 export default function Home() {
   return (
     <BackgroundImage>
-      wakawakawka
-
+        <QuizContainer>
+          waka waka e e 
+        </QuizContainer>
     </BackgroundImage>
   )
 }

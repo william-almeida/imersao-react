834f552c9d9ecda688679db1220389659e151ef7
william-almeida <william.cntt@gmail.com>
2021-01-26 01:39:14 -0300

    Widget: Content and Header

--- quiz/pages/index.js
+++ quiz/pages/index.js
@@ -20,12 +20,71 @@ export const QuizContainer = styled.div`
     padding: 15px;
   }
 `
+const Widget = styled.div`
+  margin-top: 24px;
+  margin-bottom: 24px;
+  border: 1px;
+  background-color: #FFF;
+  color: #000;
+  border-radius: 4px;
+  overflow: hidden;
+  h1, h2, h3 {
+    font-size: 16px;
+    font-weight: 700;
+    line-height: 1;
+    margin-bottom: 0;
+  }
+  p {
+    font-size: 14px;
+    font-weight: 400;
+    line-height: 1;
+  }
+`
+
+Widget.Header = styled.header`
+  display: flex;
+  justify-content: flex-start;
+  aling-items: center;
+  padding: 18px 32px;
+  background-color: ${({theme}) => theme.colors.primary};
+  * {
+    margin: 0;
+  }
+`
+
+Widget.Content = styled.div`
+  padding: 24px 32px 32px 24px;
+  & > *:first-child {
+    margin-top: 0;
+  }
+  & > *:last-child {
+    margin-bottom: 0;
+  }
+  ul {
+    list-style: none;
+    padding: 0;
+  }
+`
 
 export default function Home() {
   return (
     <BackgroundImage>
         <QuizContainer>
-          waka waka e e 
+          <Widget>
+            <Widget.Header>
+                <h1>Lil Peep</h1>
+            </Widget.Header>
+            <Widget.Content>
+              
+              <p>lorem imp askjdhkb o  poaspsh  idhf fdsijo  </p>
+            </Widget.Content>
+          </Widget>
+          <Widget>
+            <Widget.Content>
+            <h1>Lil Peep</h1>
+            <p>lorem imp askjdhkb o  po sjd jashd </p>
+            </Widget.Content>
+          </Widget>
         </QuizContainer>
     </BackgroundImage>
   )
